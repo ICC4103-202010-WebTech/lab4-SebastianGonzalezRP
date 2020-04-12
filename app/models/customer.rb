eefef7aa994082a59694bb53cbe5701e0c66d45f
SebastianGonzalezRP <38021173+SebastianGonzalezRP@users.noreply.github.com>
2020-04-12 13:42:26 -0400

    Late assignment submission

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
   has_many :orders
   has_many :tickets, through: :orders
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :name , presence: true
+  validates :lastname, presence:true
 end
